bbea9a88d67a7dc312e50b7e20166a6f4f81a09d
Carolina Rodriguez <carolinarodriguez2509@gmail.com>
2021-10-28 19:17:01 +0200

    Steps 1 and 2

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+const isImageURL = require('image-url-validator').default;
+var fs = require('fs');
 
 (async () => {
 
@@ -13,6 +16,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -20,12 +24,41 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  // > try it {{host}}/persons?name=the_name
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
+
+    //    1. validate the image_url query
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+    const is_image = await isImageURL(image_url); 
+    console.log(is_image);
+    if(! is_image ){
+      return res.status(400)
+                .send('the url must be an image');
+    }
+
+    var filtered
+    //    2. call filterImageFromURL(image_url) to filter the image
+    filterImageFromURL(image_url).then(function(filteredpath){
+      filtered = filteredpath; 
+      //    3. send the resulting file in the response
+      return res.status(200).sendFile(filteredpath);
+
+    }); 
+    console.log(filtered);
+    fs.unlinkSync(filtered);
+
+
+  } );
 
   /**************************************************************************** */
 
